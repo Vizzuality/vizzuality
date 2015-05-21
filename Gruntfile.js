f63b4a68e959e7aafa164b69e10d47d6ea739700
David Inga <david.inga@icloud.com>
2015-05-21 16:43:10 +0200

    images and project template

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,40 @@
+'use strict';
+
 var path = require('path');
 
 module.exports = function(grunt) {
 
-  'use strict';
+  var options = {
 
-  require('load-grunt-tasks')(grunt);
-
-  require('load-grunt-config')(grunt, {
     configPath: path.join(process.cwd(), 'lib', 'tasks'),
+
     init: true,
+
     data: {
       app: './app/assets',
       build: './public/assets'
     },
+
     loadGruntTasks: {
       pattern: 'grunt-*',
       config: require('./package.json'),
       scope: 'dependencies'
     }
-  });
+
+  };
+
+  require('load-grunt-tasks')(grunt);
+  require('load-grunt-config')(grunt, options);
 
   grunt.registerTask('default', ['stylus', 'symlink']);
-
-  grunt.registerTask('build', [
-    'clean', 'stylus', 'cssmin', 'copy', 'uglify', 'imagemin'
-  ]);
-
   grunt.registerTask('serve', ['stylus', 'symlink', 'concurrent']);
-
-  grunt.registerTask('deploy', ['shell:deploy']);
+  grunt.registerTask('build', [
+    'clean',
+    'stylus',
+    'cssmin',
+    'copy',
+    'uglify',
+    'imagemin'
+  ]);
 
 };
