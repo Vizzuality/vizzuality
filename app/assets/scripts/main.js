e596c6ffa097490078916113daa11fbac41f661a
Andrés González <andres.gonzalez@vizzuality.com>
2015-11-16 12:48:41 +0100

    Added dynamic parallax

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -202,6 +202,10 @@
     };
   };
 
+  utils.getScroll = function() {
+    return window.pageYOffset;
+  };
+
   utils.Template = window.t;
 
   // Function to search target and go to scroll using Smooth Scroll
@@ -483,26 +487,36 @@
 
   // Parallax effect in project page
   function doParallax() {
-    var parallaxContent = document.querySelector('.parallax-content > div');
+    var parallaxContent = document.querySelector('.parallax-bg'),
+      parallaxModule = document.querySelector('.m-parallax'),
+      parallaxBackground = document.getElementsByClassName('parallax-bg')[0].style['backgroundImage'],
+      cssBackground = 'background-image:' + parallaxBackground,
+      parallaxSize = {
+        top: parallaxModule.offsetTop,
+        bottom: parallaxModule.offsetTop + parallaxModule.offsetHeight
+      },
+      scrollY = utils.getScroll();
+
 
     if(!parallaxContent) {
       return;
     }
 
-
     var parallax = function() {
       var translateY = window.pageYOffset / 8;
 
-      this.setAttribute(
-        'style', 'transform: translate3d(0,' + translateY + 'px, 0); -webkit-transform: translate3d(0,' + translateY + 'px, 0)'
-      );
+      if(!this.style['backgroundImage']) {
+        this.style['backgroundImage'] = cssBackground;
+      }
+
+      this.style['transform'] = 'translate3d(0,' + translateY + 'px, 0)';
+      this.style['-webkit-transform'] = 'translate3d(0,' + translateY + 'px, 0)';
+      this.style['-moz-transform'] = 'translate3d(0,' + translateY + 'px, 0)';
     };
 
-    parallax.apply(parallaxContent);
-
-    window.addEventListener('scroll', function() {
+    if (scrollY > parallaxSize.top - 250 && scrollY < parallaxSize.bottom) {
       parallax.apply(parallaxContent);
-    });
+    }
   }
 
   // Navigation between projects using arrow keys
@@ -599,12 +613,15 @@
     contactForm();
     geolocationMap();
     allProjectsModal();
-    doParallax();
     arrowsNavigation();
     loadBtn();
     decodeEmail();
 
-    window.onscroll = utils.throtle(fixHeader(), 100);
+    window.addEventListener('scroll', function() {
+      utils.throtle(fixHeader(), 100);
+      doParallax();
+    });
+
   });
 
 })();
