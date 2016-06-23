80365a53af49aead65bdce244410f1012eb50ec7
David Inga <davidsingal@icloud.com>
2016-06-23 18:56:35 +0200

    improving project publish

--- app/helpers/file.js
+++ app/helpers/file.js
@@ -41,6 +41,10 @@ module.exports = {
       var filePath = path.join(dir, file);
       var result = matter(fs.readFileSync(filePath, 'utf8'));
       var element = result.data;
+      // TODO: add published in all project files and improve this statement
+      if (element.published === false) {
+        continue;
+      }
       element.slug = file.split('.md')[0];
       element.html = converter.makeHtml(result.content);
       element.grid = !element.grid ? 1 : element.grid > 3 ? 3 : element.grid;
