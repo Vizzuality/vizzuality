5334df60e5cc9877287aec866d1bdaccabd8088f
David Inga <david.inga@vizzuality.com>
2017-10-19 17:42:00 +0200

    added support for https

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -457,7 +457,7 @@
     if (elem) {
       map = L.map(elem, mapOptions);
 
-      L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+      L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
         subdomains: 'abcd',
         maxZoom: 19
