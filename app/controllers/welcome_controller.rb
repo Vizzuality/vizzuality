585484e98246542ae16090b88ed176a50005339a
Ivan Birkman <ivan.yk14@gmail.com>
2018-07-27 13:16:12 +0200

    Limit highlighted projects to five

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     projects = Project.all.where(published: true).order('weight asc')
-    @highlighted_projects = projects.where(highlighted: true)
-    @projects = projects.where(highlighted: false)
+    @highlighted_projects = projects.where(highlighted: true).limit(5)
+    @projects = projects.where.not(id: @highlighted_projects.pluck(:id))
     @clients = Client.all.where(published: true).order('name asc')
   end
 end
